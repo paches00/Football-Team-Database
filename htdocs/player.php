3ad6dd8e98a3c16dacee2f3021862a148858a919
fheshiki <70349016+paches00@users.noreply.github.com>
2021-11-21 17:48:54 +0100

    Create player.php

---
+++ htdocs/player.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Football Team Database</title>
+   <link rel="stylesheet" type="text/css" href="indexstyle.css">
+</head>
+<body>
+<h1>Player View</h1>
+<p>Select a table below to view:</p>
+<ul>
+   <li><a href = "playertable.php">Player Table</a></li>
+   <li><a href = "teamtable.php">Team Table</a></li>
+   <li><a href = "seasontable.php">Season Table</a></li>
+   <li><a href = "matchtable.php">Match Table</a></li>
+</ul>
+</body>
+</html>
