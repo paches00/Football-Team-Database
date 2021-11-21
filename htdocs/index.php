1d23df6a8e5bcc74733bbc50e4835988318254fb
fheshiki <70349016+paches00@users.noreply.github.com>
2021-11-21 17:48:42 +0100

    Create index.php

---
+++ htdocs/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Football Team Database</title>
+   <link rel="stylesheet" type="text/css" href="indexstyle.css">
+</head>
+<body>
+<h1>Football Team Database</h1>
+<p>Welcome to the Football Team Database!</p>
+<p>Select a user below to view the data:</p>
+<ul>
+   <li><a href = "owner.php">Owner</a></li>
+   <li><a href = "director.php">Director</a></li>
+   <li><a href = "coach.php">Coach</a></li>
+   <li><a href = "analytics.php">Analytics Department</a></li>
+   <li><a href = "player.php">Player</a></li>
+</ul>
+</body>
+</html>
