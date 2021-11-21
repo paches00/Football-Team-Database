bd77c5497a052c00a70e32ca4cd22b87d80b409d
fheshiki <70349016+paches00@users.noreply.github.com>
2021-11-21 17:48:09 +0100

    Create analytics.php

---
+++ htdocs/analytics.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Football Team Database</title>
+   <link rel="stylesheet" type="text/css" href="indexstyle.css">
+</head>
+<body>
+<h1>Analytics Department View</h1>
+<p>Select a table below to view:</p>
+<ul>
+   <li><a href = "playertable.php">Player Table</a></li>
+   <li><a href = "positiontable.php">Position Table</a></li>
+   <li><a href = "amateurtable.php">Amateur Table</a></li>
+   <li><a href = "teamtable.php">Team Table</a></li>
+   <li><a href = "seasontable.php">Season Table</a></li>
+   <li><a href = "matchtable.php">Match Table</a></li>
+</ul>
+</body>
+</html>
